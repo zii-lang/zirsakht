11423203aff91a7357075cc7e59d125d2e064d99
Caspian <caspianrz@gmail.com>
2026-07-11 20:03:45 +0330

    add: improve SourceFile class with file mapping.
    * added map, unmap, open_file, close_file functions for windows.

--- include/Z/IO/SourceFile.hpp
+++ include/Z/IO/SourceFile.hpp
@@ -9,6 +9,7 @@
 #include <filesystem>
 #include <string>
 #include <string_view>
+#include <vector>
 
 namespace Z::IO {
     struct SourceFile {
@@ -22,22 +23,37 @@ namespace Z::IO {
         SourceFile(SourceFile &&) noexcept;
         SourceFile &operator=(SourceFile &&) noexcept;
 
-        std::string_view view() const noexcept {
-            return nullptr;
-        }
+        std::string_view view() const noexcept;
 
-        const std::filesystem::path &path() const noexcept {
-            return path_;
+        const std::filesystem::path &get_path() const noexcept {
+            return this->path;
         }
 
       private:
-        std::filesystem::path path_;
+        std::filesystem::path path;
+        const char           *data = nullptr;
+        std::size_t           size = 0;
+
+        std::vector<std::size_t> line_offsets;
+        bool                     file_changed = true;
+
+        // ==== Private Functions ====
+
+        void build_line_table();
+
+        void open_file();
+        void close_file() noexcept;
+
+        void map();
+        void unmap() noexcept;
+
+        // ==========================
 
 #ifdef _WIN32
-        HANDLE file_    = INVALID_HANDLE_VALUE;
-        HANDLE mapping_ = nullptr;
+        HANDLE file    = INVALID_HANDLE_VALUE;
+        HANDLE mapping = nullptr;
 #else
-        int file_ = -1;
+        int file = -1;
 #endif
     };
 } // namespace Z::IO
--- src/SourceFile.cpp
+++ src/SourceFile.cpp
@@ -1,12 +1,90 @@
 #include <Z/IO/SourceFile.hpp>
 
 namespace Z::IO {
-    SourceFile::SourceFile(const std::filesystem::path &path) : path_(path) {
+    SourceFile::SourceFile(const std::filesystem::path &path) : path(path) {
+    }
 
+    void SourceFile::open_file() {
 #ifdef _WIN32
+        if (INVALID_HANDLE_VALUE != this->file)
+            return;
 
-#else
+        this->file =
+            CreateFileW(this->get_path().c_str(), GENERIC_READ, FILE_SHARE_READ,
+                        nullptr, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, nullptr);
 
+        if (INVALID_HANDLE_VALUE == this->file) {
+            throw std::system_error(GetLastError(), std::system_category());
+        }
+
+        LARGE_INTEGER size;
+
+        if (!GetFileSizeEx(this->file, &size)) {
+            this->close_file();
+            throw std::system_error(GetLastError(), std::system_category());
+        }
+
+        this->size = static_cast<std::size_t>(size.QuadPart);
+#endif
+    }
+
+    void SourceFile::close_file() noexcept {
+#ifdef _WIN32
+        if (INVALID_HANDLE_VALUE != this->file) {
+            CloseHandle(this->file);
+            this->file = INVALID_HANDLE_VALUE;
+        }
+
+        this->size = 0;
+#endif
+    }
+
+    void SourceFile::map() {
+        if (this->data) {
+            return;
+        }
+
+#if _WIN32
+        if (INVALID_HANDLE_VALUE == this->file) {
+            this->open_file();
+        }
+#endif
+
+        if (0 == this->size) {
+            return;
+        }
+
+#if _WIN32
+        this->mapping = CreateFileMappingW(this->file, nullptr, PAGE_READONLY,
+                                           0, 0, nullptr);
+
+        if (!this->mapping) {
+            throw std::system_error(GetLastError(), std::system_category());
+        }
+
+        this->data = static_cast<const char *>(
+            MapViewOfFile(this->mapping, FILE_MAP_READ, 0, 0, 0));
+
+        if (!this->data) {
+            CloseHandle(this->mapping);
+            this->mapping = nullptr;
+
+            throw std::system_error(GetLastError(), std::system_category());
+        }
+#endif
+    }
+
+    void SourceFile::unmap() {
+#if _WIN32
+        if (this->data) {
+            UnmapViewOfFile(this->data);
+            this->data = nullptr;
+        }
+
+        if (this->mapping) {
+            CloseHandle(this->mapping);
+            this->mapping = nullptr;
+        }
 #endif
     }
 } // namespace Z::IO
