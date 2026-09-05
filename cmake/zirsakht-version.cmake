bb8f229370617f7ab724ca359adb449beb9630a4
Caspian <caspianrz@gmail.com>
2026-09-05 11:37:05 +0330

    fix version in linux/mac builds.

---
+++ cmake/zirsakht-version.cmake
@@ -0,0 +1,9 @@
+set(zirsakht_version_major 1)
+set(zirsakht_version_minor 0)
+set(zirsakht_version_patch 0)
+
+set(zirsakht_version
+	"${zirsakht_version_major}.${zirsakht_version_minor}.${zirsakht_version_patch}"
+)
+
+set(PACKAGE_VEERSION ${zirsakht_version})
